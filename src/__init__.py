659a8ea858ac7ef2bfb3735e03fc6dd0a28ca382
Craig Bourne <craigabourne@gmail.com>
2025-07-12 13:38:30 +0100

    init commit

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Attack Tree Visualiser for InfoSec Assignment 2
+
+A Python application for visualising and analysing attack trees 
+for Pampered Pets risk assessment.
+"""
+
+__version__ = "0.1.0"
